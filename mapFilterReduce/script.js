452fa16c817474128b39d930f099a4c0a727b6c3
sameep-s <sameep.sharma98@gmail.com>
2022-01-06 22:51:01 +0530

    firstJsCommit

---
+++ mapFilterReduce/script.js
@@ -0,0 +1,51 @@
+// map, reduce and filter
+
+// ----------------------------- MAP -----------------------
+const arr = [5, 1, 3, 2, 6];
+
+// Double - [10,2,6,4,12]
+
+function double(x) {
+    return x * 3;
+}
+
+function binary(x) {
+    return x.toString(2);
+}
+
+const output = arr.map(binary);
+
+const op2 = arr.map((x) => x * 21);
+
+
+console.log(output);
+console.log(op2);
+
+// -------------------------- FILTER ----------------------
+
+// filter odd values
+function isOdd(x) {
+    return x > 4;
+}
+
+const op3 = arr.filter(isOdd);
+
+console.log(op3);
+
+// ----------------------------- REDUCE --------------------
+
+// to find sum or max number
+
+const output4 = arr.reduce((acc, cur) => {
+    return acc = acc + cur;
+}, 0);
+// 0 is the second parameter of the reduce function,
+//  it is the initial value of accumulator
+
+const op5 = arr.reduce((acc, cur) => {
+    return (cur > acc ? acc = cur : acc = acc);
+}, 0)
+
+// printing on console
+console.log(output4);
+console.log(op5);
