f391000bd27795a6d9c0dacb17f7676993ff87d2
sameep-s <sameep.sharma98@gmail.com>
2022-01-08 22:52:09 +0530

    iife-commit

--- randomJs/script.js
+++ randomJs/script.js
@@ -1,3 +1,8 @@
 // this if for iife functions
+// immediately invoked function expressions
 
-()
+
+// it has no name and is invoked immediately.
+(() => {
+    console.log("this is an iife function");
+})();
