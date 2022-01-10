20a0ef26be3a2c7c2928ca66a4e32103abe3c59e
sameep-s <sameep.sharma98@gmail.com>
2022-01-10 16:58:46 +0530

    10 jan

--- randomJs/script.js
+++ randomJs/script.js
@@ -26,3 +26,30 @@ let labels = [];
 repeat(5, (j) => labels.push(`item ${j}`));
 
 console.log(labels);
+
+
+// new Function to tell greater than 10
+// higher order function returning function
+
+function greaterThan(n) {
+    return ((m) => m > n);
+}
+
+let greaterThan10 = greaterThan(10);
+let greaterThan21 = greaterThan(21);
+
+console.log(greaterThan10());
+console.log(greaterThan10(11));
+
+console.log(greaterThan21(24));
+
+function isOne(num) {
+    return num === 1;
+}
+
+
+function isBigger(a, b) {
+    return a > b;
+}
+
+const Add22andReturn = num => num + 22;
