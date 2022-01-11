3a0ec7ec38246bdef710ff65897276fa21433efb
sameep-s <sameep.sharma98@gmail.com>
2022-01-11 20:02:31 +0530

    arrow V/s normal functions

--- 01_10_2022_javascriptExercises/script.js
+++ 01_10_2022_javascriptExercises/script.js
@@ -156,19 +156,32 @@ const sam = foo ?? 24;
 console.log(sam);
 
 
-*/
-
-
 
 // Some examples
 
 // NOTE: cannot chian nullish coalescing operator with AND or OR operators.
 
 let foo;
-
 let dummyText = foo || 'Hello';
-console.log(dummyText);
 
+console.log(`dummyText`);
+
+*/
+
+// -------------------------- ARROW FUNCTION V/S NORMAL FUNCTIONS
+
+let user = {
+    name: "GFG",
+    gfg1: () => {
+        console.log("hello " + this.name); // no 'this' binding here
+    },
+    gfg2() {
+        console.log("Welcome to " + this.name); // 'this' binding works here
+    }
+};
+
+user.gfg1();
+user.gfg2();
 
 
 
