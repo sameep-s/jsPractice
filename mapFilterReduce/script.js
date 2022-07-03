106c1adebb4b66e7b4214520aac513f31bb99696
sameep-s <sameep.sharma98@gmail.com>
2022-07-03 15:27:48 +0530

    arrayReduce polyfill

--- mapFilterReduce/script.js
+++ mapFilterReduce/script.js
@@ -601,7 +601,7 @@ console.log(`sumCurry:`, sumCurry(234)(3422)());
 
 // ------------------MAP
 
-const myArr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+const myArr = [1, 1, 2, 3, 4, 5, 5, 6, 7, 8, 2, 5, 3, 3, 2, 2, 4, 45, 6, 787, 5, 2, 3, 4, 5, 6, 7, 8, 9];
 
 Array.prototype.myMap = function (cb) {
     console.log(this);
@@ -632,13 +632,36 @@ Array.prototype.myFilter = function (cb) {
     return newArr;
 }
 
-const element = myArr.myFilter((i) => i === 2);
-console.log(`element:`, element);
+const element = myArr.myFilter((i) => i !== 2);
+// console.log(`element:`, element);
 
 
 
 
-// ------------------ Reduce
+// ------------------ Reduce-polyfill
+
+// arr.reduce ((acc,curr,i,arr)=>{}, initialValue);
+
+
+Array.prototype.myReduce = function (cb, initialValue) {
+    let accumulator = initialValue;
+
+    for (let i = 0; i < this.length; i++) {
+        accumulator ? cb(accumulator, this[i], i, this) : this[i];
+    }
+
+    return accumulator;
+}
+
+
+const arrReduce = myArr.myReduce((acc, curr) => {
+
+    acc[curr] !== undefined ? acc[curr] += 1 : acc[curr] = 1;
+
+    return acc;
+}, {});
+
+console.log(`arrayReduce: `, arrReduce);
 
 
 
